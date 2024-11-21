86dcee0c16736509d1fd26ebab3ac7caa2f266f9
Arshal Abbas <arshalabbas123@gmail.com>
2024-11-21 17:44:56 +0530

    Implemented the drawer and fixed some layout issues

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const header = document.querySelector("header");
 const locationMark = document.querySelector("#location-mark");
 
+const drawerWrapper = document.querySelector("#drawer-wrapper");
+const drawer = document.querySelector("aside");
+
 window.addEventListener("scroll", (e) => {
   header.classList.toggle("light-header", window.scrollY !== 0);
   locationMark.classList.toggle(
@@ -9,3 +12,13 @@ window.addEventListener("scroll", (e) => {
   );
   console.log(window.scrollY);
 });
+
+function openDrawer() {
+  drawerWrapper.classList.toggle("open");
+  drawer.classList.toggle("open");
+}
+
+function closeDrawer() {
+  drawerWrapper.classList.remove("open");
+  drawer.classList.remove("open");
+}
